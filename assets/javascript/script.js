bed291714647ef40f605887a2969fdff06f3db78
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-01-02 22:00:37 -0800

    addcounter

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -34,9 +34,9 @@
         document.querySelector("#word").innerHTML = '';
 
         // Hide 'play again' button.
-        $reset.classList.add('hide')
+        $reset.classList.add('hide');
 
-
+        guesses = 0;
 
         // Pick a random word from our array.
         word = words[Math.floor(Math.random() * words.length)];
@@ -57,9 +57,7 @@
 
     }
 
-    function checkNumberOfGuesses(guesses) {
-        // Reset global guess count.
-        guesses = 0;
+    function updateNumberOfGuesses() {
         //Display number of guesses made and the directions
         document.querySelector("#guesses").innerHTML = "Number of Guesses: " + guesses;
         document.querySelector("#word").innerHTML = "Press any key to guess an empty spot.  Be careful though!  You only get 7 chances before it is game over!";
@@ -69,16 +67,18 @@
             document.querySelector("#guesses").innerHTML = "Game Over!";
             document.querySelector("#word").innerHTML = "Good try " + name + "!  The word was " + word;
 
-            $reset.classList.remove('hide')
+            $reset.classList.remove('hide');
 
         }
-    };
+    }
 
 
     function checkGuess(guess) {
         //Obtain guess from user
         //Forces all characters to be lower case
         guess = guess.toLowerCase();
+
+        var isValid = false;
         //Update the array with the guess
         for (var j = 0; j < word.length; j++) {
             // If word letter is same as user guess
@@ -87,8 +87,14 @@
                 answerArray[j] !== guess) {
                 answerArray[j] = guess;
                 remainingLetters--;
+                isValid = true;
             }
         }
+
+        if (isValid === false) {
+            guesses++
+        }
+        updateNumberOfGuesses ()
 
         return answerArray;
 
